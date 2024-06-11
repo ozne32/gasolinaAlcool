9438f0e03268706c815ba8e325e3f3c3a5c04eb1
ozne32 <enzorcc.mf3rs@gmail.com>
2024-06-11 15:57:13 -0300

    .

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  // como o typescript é fortemente tipado, para eu conseguir
+  // receber qualquer valor e dps tratar ele melhor
+  //eu posso fazer isso
+  public precoAlcool: any;
+  public precoGasolina: any;
+  public resultado: String=''
+  calcular(){
+      // verificar se foi preenchido 
+      if(this.precoAlcool && this.precoGasolina){
+        let alcool = parseFloat(this.precoAlcool)
+        let gasolina= parseFloat(this.precoGasolina)
+        if(alcool/gasolina >=0.7){
+          this.resultado = 'melhor utilizar gasolina'
+        }else{
+          this.resultado='melhor utilizar o álcool'
+        }
+      }else{
+        this.resultado="não preenchido"
+      }
+  }
   constructor() {}
 
 }
